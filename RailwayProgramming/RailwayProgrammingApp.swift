8368a7f2974bdeb78fad7265df096c440131c6b3
Razvan Litianu <litianu.razvan@gmail.com>
2022-06-23 17:55:57 +0300

    Initial Commit

---
+++ RailwayProgramming/RailwayProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RailwayProgrammingApp.swift
+//  RailwayProgramming
+//
+//  Created by Razvan on 23.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct RailwayProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
